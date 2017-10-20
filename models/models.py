27b92b09984797df221fa1a65f76f35bc5d5c79f
lorne <lorne@horstwelding.com>
2017-10-20 15:46:38 -0400

    Working Views of related data.

--- models/models.py
+++ models/models.py
@@ -8,6 +8,8 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     notes = fields.Text()
+    
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
 
 
 class Session(models.Model):
@@ -17,5 +19,9 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    
     
 
